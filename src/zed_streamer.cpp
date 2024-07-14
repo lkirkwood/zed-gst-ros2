683ef8627f37ba52613f45a826ec02d927fe6fd1
Linus Kirkwood <linuskirkwood@gmail.com>
2024-07-15 03:32:49 +1000

    add basic callback

--- src/zed_streamer.cpp
+++ src/zed_streamer.cpp
@@ -1,32 +1,50 @@
 #include "zed-gst/zed_streamer.hpp"
+#include "gst/gst.h"
+#include "rclcpp_lifecycle/state.hpp"
 #include "utsma_common/lifecycle_node.hpp"
-#include <rclcpp_lifecycle/state.hpp>
+#include <rclcpp/logger.hpp>
+#include <rclcpp/logging.hpp>
 
 ZedStreamer::ZedStreamer()
-    : utsma_common::LifecycleNode("zed-cam", "", 1000.0, true), active(false),
-      logger(std::make_shared<ROSLogger>(2000000000ULL)) {
-  logger->log_debug("zed-cam initialised.");
+    : utsma_common::LifecycleNode("zed_cam", "", 1000.0, false) {
+  timer =
+      this->create_wall_timer(500ms, std::bind(&ZedStreamer::callback, this));
+
+  RCLCPP_DEBUG(this->get_logger(), "Streamer initialised.");
 }
 
-utsma_common::CallbackReturn on_configure(const rclcpp_lifecycle::State &) {
+ZedStreamer::~ZedStreamer() {}
+
+utsma_common::CallbackReturn
+ZedStreamer::on_configure(const rclcpp_lifecycle::State &) {
+  GError *error = new GError;
+  if (gst_init_check(NULL, NULL, &error)) {
+    return utsma_common::CallbackReturn::SUCCESS;
+  } else {
+    std::string message = "Failed to initialise gstreamer: ";
+    message += error->message;
+    return utsma_common::CallbackReturn::ERROR;
+  }
+};
+
+utsma_common::CallbackReturn
+ZedStreamer::on_activate(const rclcpp_lifecycle::State &) {
   return utsma_common::CallbackReturn::SUCCESS;
 };
 
-utsma_common::CallbackReturn on_activate(const rclcpp_lifecycle::State &) {
+utsma_common::CallbackReturn
+ZedStreamer::on_deactivate(const rclcpp_lifecycle::State &) {
   return utsma_common::CallbackReturn::SUCCESS;
 };
 
-utsma_common::CallbackReturn on_deactivate(const rclcpp_lifecycle::State &) {
+utsma_common::CallbackReturn
+ZedStreamer::on_cleanup(const rclcpp_lifecycle::State &) {
   return utsma_common::CallbackReturn::SUCCESS;
 };
 
-utsma_common::CallbackReturn on_cleanup(const rclcpp_lifecycle::State &) {
+utsma_common::CallbackReturn
+ZedStreamer::on_shutdown(const rclcpp_lifecycle::State &) {
   return utsma_common::CallbackReturn::SUCCESS;
 };
 
-utsma_common::CallbackReturn on_shutdown(const rclcpp_lifecycle::State &) {
-  return utsma_common::CallbackReturn::SUCCESS;
-};
-
-void process(){};
-void create_publishers(){};
+void ZedStreamer::callback() { RCLCPP_INFO(this->get_logger(), "Callback."); }
