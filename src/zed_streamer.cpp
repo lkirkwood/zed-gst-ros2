31749edf8215ae84f289006716b88f9e3ad221f9
Linus Kirkwood <linuskirkwood@gmail.com>
2024-07-14 22:53:41 +1000

    added dummy impls for all lifecycle node methods

--- src/zed_streamer.cpp
+++ src/zed_streamer.cpp
@@ -1,5 +1,32 @@
 #include "zed-gst/zed_streamer.hpp"
+#include "utsma_common/lifecycle_node.hpp"
+#include <rclcpp_lifecycle/state.hpp>
 
-class ZedStreamer : public utsma_common::LifecycleNode {
-public:
+ZedStreamer::ZedStreamer()
+    : utsma_common::LifecycleNode("zed-cam", "", 1000.0, true), active(false),
+      logger(std::make_shared<ROSLogger>(2000000000ULL)) {
+  logger->log_debug("zed-cam initialised.");
+}
+
+utsma_common::CallbackReturn on_configure(const rclcpp_lifecycle::State &) {
+  return utsma_common::CallbackReturn::SUCCESS;
 };
+
+utsma_common::CallbackReturn on_activate(const rclcpp_lifecycle::State &) {
+  return utsma_common::CallbackReturn::SUCCESS;
+};
+
+utsma_common::CallbackReturn on_deactivate(const rclcpp_lifecycle::State &) {
+  return utsma_common::CallbackReturn::SUCCESS;
+};
+
+utsma_common::CallbackReturn on_cleanup(const rclcpp_lifecycle::State &) {
+  return utsma_common::CallbackReturn::SUCCESS;
+};
+
+utsma_common::CallbackReturn on_shutdown(const rclcpp_lifecycle::State &) {
+  return utsma_common::CallbackReturn::SUCCESS;
+};
+
+void process(){};
+void create_publishers(){};
